3b33b1eb796ae1fadaa797d68e9002da12cebd56
Henrik Strötgen <hstroetgen@synapticon.com>
2016-11-25 17:08:21 +0100

    add missing argument error

--- Flash_Contelec/flash_contelec.sh
+++ Flash_Contelec/flash_contelec.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Usage: ./flash_contelec <BIN-FILE>
 
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    echo "Call ./flash_contelec.sh <binary>"
+    exit 1
+fi
+
 BIN=$1
 CONTELEC_DEV="LPC1112"
 
