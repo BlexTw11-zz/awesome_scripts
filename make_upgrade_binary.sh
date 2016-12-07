2cf66192e1af76eaac01271423a943fe77c5d175
Henrik Strötgen <hstroetgen@synapticon.com>
2016-12-07 18:50:35 +0100

    add unique name for output file

--- make_upgrade_binary.sh
+++ make_upgrade_binary.sh
@@ -6,8 +6,15 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
-    echo "Call ./make_upgrade_binary.sh </path/to/file.xe>"
+    echo "Call ./make_upgrade_binary.sh </path/to/file.xe>i [<output_name>]"
     exit 1
+fi
+
+#INPUT_NAME=""
+
+if [ $# -eq 2 ]
+  then
+    INPUT_NAME=$2
 fi
 
 XTIMECOMPOSER=14.2
@@ -16,6 +23,13 @@ BOOTPARTITION_SIZE=0x30000
 PATH_FILE=$1
 FILE=${PATH_FILE##*/}
 NOW=$(date +"%d%m%y-%H%M%S")
-NAME="${FILE%.xe}-$NOW.bin"
+
+if [ $INPUT_NAME ]; then
+    NAME="$INPUT_NAME-$NOW.bin"
+else
+    NAME="${FILE%.xe}-$NOW.bin"
+fi
+
+echo "Name:" $NAME
 
 xflash --factory-version $XTIMECOMPOSER --upgrade 1 $PATH_FILE --boot-partition-size $BOOTPARTITION_SIZE -o $NAME
