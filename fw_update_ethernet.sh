1eda27340afed05850f951db479b7a762b50662b
Henrik Strötgen <hstroetgen@synapticon.com>
2016-12-15 09:34:37 +0100

    fix ip settings, add comments

--- fw_update_ethernet.sh
+++ fw_update_ethernet.sh
@@ -6,22 +6,26 @@
 
 NODE_IP=192.168.0.1
 MASTER_IP=192.168.0.2
-TFTP=`which tftp`
 
+# Check if tftp is installed
+TFTP=`which tftp`
 if [ -z ${TFTP} ]
 then
    echo "Error: tftp is not installed"
    exit 1
 fi
 
+# Check if path to binary is supplied
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
-    echo "Call ./make_ethernet_update.sh <binary>"
+    echo "Call ./make_ethernet_update.sh </path/to/binary>"
     exit 1
 fi
 
-IF_NAME=`ifconfig | grep -oe "^e[nt][a-z0-9]*"
+IF_NAME=`ifconfig | grep -oe "^e[nt][a-z0-9]*"`
+for i in $IF_NAME; do lastInterface=$i; done;
+IF_NAME=$lastInterface
 
 # Set temporary new IP address`
 `sudo ifconfig $IF_NAME $MASTER_IP`
@@ -33,5 +37,8 @@ mode binary
 put  $BINARY
 quit
 !
+
 # Reset IP address from interface
 `sudo ifconfig $IF_NAME 0.0.0.0`
+# Don't know if necessary
+`sudo dhclient $IF_NAME`
