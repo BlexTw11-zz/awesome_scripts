195a4dcd3ec598bb722fa8882349e62b3fabbe6a
Henrik Strötgen <hstroetgen@synapticon.com>
2017-02-03 10:19:41 +0100

    rename xflash_delete to targets

--- xflash_delete.sh
+++ xflash_delete.sh
@@ -4,6 +4,7 @@
 ################################################
 
 TARGET="SOMANET-C22.xn"
+TARGET_PATH="targets"
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -11,4 +12,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 echo "Delete target: " $TARGET
 
 # Erase chip
-xflash --erase-all --target-file $SCRIPTPATH/$TARGET
+xflash --erase-all --target-file $SCRIPTPATH/$TARGET_PATH/$TARGET
