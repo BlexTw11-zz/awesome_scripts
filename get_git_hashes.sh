ed7b9e80686a184b4f32b122c411eb40e036296e
Henrik Strötgen <hstroetgen@synapticon.com>
2017-06-09 10:05:28 +0200

    get_git_hashes: remove error prompt

--- get_git_hashes.sh
+++ get_git_hashes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -maxdepth 1 -type d -exec echo -e "\033[7m" {} ":\033[0m" \; -exec git -C {} rev-parse HEAD \;
+find . -maxdepth 2 -type d -name ".git" -exec dirname {} \; -exec git -C {} rev-parse -q HEAD \; -exec echo \;
