8c56f808ec665eaa10494d85be32c971175f95e7
BlexTw11 <roadtrain@gmx.de>
2017-06-10 11:06:48 +0200

    add functionallity for C21

--- xflash_delete.sh
+++ xflash_delete.sh
@@ -3,8 +3,30 @@
 # Erase your CORE module with this nice script #
 ################################################
 
-TARGET="SOMANET-C22.xn"
-TARGET_PATH="targets"
+TARGET_PATH="target"
+
+
+if [ $# -eq 0 ] then
+    echo "No target supplied"
+    echo "Call $0 <target>"
+    exit 1
+fi
+
+TARGET=$1
+
+if [ $TARGET != "C22" && $TARGET != "C21" ] then
+    echo "Wrong target"
+    echo "Use 'C22' or 'C21'"
+    exit 1
+fi 
+
+
+if [ $TARGET == "C22" ] then
+    TARGET_FILE="SOMANET-C22.xn"
+elif [ $TARGET == "C21" ] then
+    TARGET_FILE="SOMANET-C21-DX_G2.xn"
+fi
+
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -12,4 +34,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 echo "Delete target: " $TARGET
 
 # Erase chip
-xflash --erase-all --target-file $SCRIPTPATH/$TARGET_PATH/$TARGET
+xflash --erase-all --target-file $SCRIPTPATH/$TARGET_PATH/$TARGET_FILE
+echo "done...exit"
