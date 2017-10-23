e6f822317e740ea7a6541d541a119fa8219a2a23
Henrik Strötgen <hstroetgen@synapticon.com>
2017-10-23 12:29:11 +0200

    init_workspace: adapt for new name scheme

--- init_workspace.py
+++ init_workspace.py
@@ -12,43 +12,37 @@ import re
 import argparse
 
 com_dict = {
-    'ecat': 'COM_ECAT-rev-a.bsp',
-    'enet': 'COM_ETHERNET-rev-a.bsp',
-    'serial': 'COM_SERIAL-rev-a.bsp',
+    'ecat': 'ComEtherCAT-rev-a.bsp',
+    'enet': 'ComEthernet-rev-a.bsp',
+    'serial': 'ComSerial-rev-a.bsp',
 }
 
 core_dict = {
-    'c21a': 'CORE_C21-rev-a.bsp',
-    'c21b': 'CORE_C21-rev-b.bsp',
-    'c21g2': 'CORE_C21-DX_G2.bsp',
-
-    'c22a': 'CORE_C22-rev-a.bsp',    
+    'c21': 'CoreC21.bsp',
+    'c21b': 'CoreC21-rev-b.bsp',
+    'c22': 'CoreC22.bsp',
+    'c2x': 'CoreC2X.bsp',
 }
 
 
-ifm_dict = {
+drive_dict = {
     
-    'dc100a': 'IFM_DC100-rev-a.bsp',
-    'dc100b': 'IFM_DC100-rev-b.bsp',
+    'd100a': 'Drive100.bsp',
 
-    'dc1kc1': 'IFM_DC1K-rev-c1.bsp',
-    'dc1kc2': 'IFM_DC1K-rev-c2.bsp',
-    'dc1kc3': 'IFM_DC1K-rev-c3.bsp',
-    'dc1kc4': 'IFM_DC1K-rev-c4.bsp',
-    'dc1kd1': 'IFM_DC1K-rev-d1.bsp',
+    'd1000c4': 'Drive1000-rev-c4.bsp',
+    'd1000d1': 'Drive1000-rev-d1.bsp',
 
-    'dc30a': 'IFM_DC30-rev-a.bsp',
+    'd50duo': 'Drive50Duo-rev-a.bsp',
 
-    'dc300a': 'IFM_DC300-rev-a.bsp',
-
-    'dc500a': 'IFM_DC500-rev-a1.bsp',
-    'dc5ka3': 'IFM_DC5K-rev-A3.bsp',
+    'd500': 'Drive500-rev-a1.bsp',
+    'd5000a3': 'Drive5000-rev-a3.bsp',
 }
 
 target_dict = {
-    'c22': 'SOMANET-C22',
-    'c21': 'SOMANET-C21-DX',
-    'c21g2': 'SOMANET-C21-DX_G2',
+    'c22': 'SOMANET-CoreC22',
+    'c21': 'SOMANET-CoreC21',
+    'c21b': 'SOMANET-CoreC21-rev-b',
+    'c2x':  'SOMANET-CoreC2X',
 }
 
 
@@ -56,7 +50,7 @@ arg_parser = argparse.ArgumentParser(description='Synapticon SOMANET workspace i
 arg_parser.add_argument('-p', '--path', help='Path to repository', dest='path')
 arg_parser.add_argument('-com', default='', help='COM module BSP', dest='com')
 arg_parser.add_argument('-core', default='c22a', help='CORE module BSP', dest='core')
-arg_parser.add_argument('-ifm', default='', help='IFM module BSP', dest='ifm')
+arg_parser.add_argument('-drive', default='', help='IFM module BSP', dest='drive')
 
 args = arg_parser.parse_args()
 
@@ -64,7 +58,7 @@ args = arg_parser.parse_args()
 path = args.path
 com_bsp = args.com
 core_bsp = args.core
-ifm_bsp = args.ifm
+drive_bsp = args.drive
 
 err = False
 if not com_bsp in com_dict and com_bsp: 
@@ -75,28 +69,27 @@ if not core_bsp in core_dict and core_bsp:
     print 'Wrong CORE module'
     print core_dict
     err = True
-if not ifm_bsp in ifm_dict and ifm_bsp:
+if not drive_bsp in drive_dict and drive_bsp:
     print 'Wrong IFM module'
-    print ifm_dict
+    print drive_dict
     err = True
-if not ifm_bsp:
+if not drive_bsp:
     print 'No IFM module'
     err = True
+
+if not core_bsp in target_dict:
+    print 'Wrong target'
+    print core_bsp
+    err = True
+
 if err:
     arg_parser.print_help()
     sys.exit(1)
 
-if core_bsp == 'c22a':
-    target = 'SOMANET-C22'
-elif core_bsp == 'c21a' or core_bsp == 'c21b':
-    target = 'SOMANET-C21-DX'
-elif core_bsp == 'c21g2':
-    target = 'SOMANET-C21-DX_G2'
-
 re_com = re.compile(r'\#include.+\<COM_.+\>')
 re_core = re.compile(r'\#include.+\<CORE_.+\>')
-re_ifm = re.compile(r'\#include.+\<IFM_.+\>')
-re_target = re.compile(r'TARGET = ?(SOMANET-C22|SOMANET-C21-DX|SOMANET-C21-DX_G2|)\n')
+re_drive = re.compile(r'\#include.+\<DRIVE_.+\>')
+re_target = re.compile(r'TARGET = ?(SOMANET-CoreC22|SOMANET-CoreC21|SOMANET-CoreC21-rev-b|SOMANET-CoreC2X|)\n')
 
 for root, dirs, files in os.walk(path):
     for name in files:
@@ -110,17 +103,17 @@ for root, dirs, files in os.walk(path):
                 f.close()
                 if core_bsp:
                     f_txt = re_core.sub('#include <'+core_dict[core_bsp]+'>', f_txt)
-                    print 'CORE', core_bsp
+                    print 'Core', core_bsp
 
-                if ifm_bsp:
-                    f_txt = re_ifm.sub('#include <'+ifm_dict[ifm_bsp]+'>', f_txt)
-                    print 'IFM', ifm_bsp
+                if drive_bsp:
+                    f_txt = re_drive.sub('#include <'+drive_dict[drive_bsp]+'>', f_txt)
+                    print 'Drive', drive_bsp
 
                 if com_bsp:
                     com_found = re_com.search(f_txt)
                     if com_found:
                         f_txt = re_com.sub('#include <'+com_dict[com_bsp]+'>', f_txt)
-                        print 'Replace COM', com_bsp
+                        print 'Replace Com', com_bsp
                     else:
                         #f = open(file_name, 'r')
                         #f_txt = f.readlines()
@@ -128,7 +121,7 @@ for root, dirs, files in os.walk(path):
                         for i in range(len(f_txt)):
                             if re_core.search(f_txt[i]):
                                 f_txt.insert(i, '#include <'+com_dict[com_bsp]+'>\n')
-                                print 'Insert COM', com_bsp
+                                print 'Insert Com', com_bsp
                                 break
                         f_txt = ''.join(f_txt)
 
@@ -148,8 +141,8 @@ for root, dirs, files in os.walk(path):
                 target_found = re_target.search(f_txt)
 
                 if target_found:
-                    f_txt = re_target.sub('TARGET = %s\n' % target, f_txt)
-                    print 'Insert Target', target
+                    f_txt = re_target.sub('TARGET = %s\n' % target_dict[core_bsp], f_txt)
+                    print 'Insert Target', target_dict[core_bsp]
                     f = open(os.path.join(root, name), 'w')
                     f.write(f_txt)
                     f.close()
